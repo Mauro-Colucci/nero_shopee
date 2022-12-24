7dbdc620cff4dd581150822b3ca6ca28be7909c4
Mauro Colucci <colucci.mauro@gmail.com>
2022-12-24 17:33:36 -0300

    navbar and state

--- client/src/App.jsx
+++ client/src/App.jsx
@@ -1,5 +1,41 @@
+import { useEffect } from "react";
+import {
+  BrowserRouter,
+  Routes,
+  Route,
+  useLocation,
+  //  ScrollRestoration,
+} from "react-router-dom";
+import Home from "./scenes/home/Home";
+import ItemDetails from "./scenes/itemDetails/itemDetails";
+import Checkout from "./scenes/checkout/Checkout";
+import Confirmation from "./scenes/checkout/Confirmation";
+import Navbar from "./scenes/global/Navbar";
+
+//test scroll restoration instead
+const ScrollToTop = () => {
+  const { pathname } = useLocation();
+  useEffect(() => {
+    window.scrollTo(0, 0);
+  }, [pathname]);
+  return null;
+};
+
 function App() {
-  return <div className="app">chello</div>;
+  return (
+    <div className="app">
+      <BrowserRouter>
+        <Navbar />
+        <ScrollToTop />
+        <Routes>
+          <Route path="/" element={<Home />} />
+          <Route path="/item/:itemId" element={<ItemDetails />} />
+          <Route path="/checkout" element={<Checkout />} />
+          <Route path="/checkout/success" element={<Confirmation />} />
+        </Routes>
+      </BrowserRouter>
+    </div>
+  );
 }
 
 export default App;
